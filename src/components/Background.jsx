2e1d95c8aaf317b0bb909d84f5e22c5ca99d4b71
Leonardo Moraes <leosmf07@hotmail.com>
2026-08-06 18:14:33 -0300

    Delete src directory

--- src/components/Background.jsx
+++
@@ -1,8 +0,0 @@
-export default function Background() {
-  return (
-    <div className="page-bg" aria-hidden="true">
-      <div className="page-bg__grid" />
-      <div className="page-bg__noise" />
-    </div>
-  )
-}
